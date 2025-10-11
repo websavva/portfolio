9e35ac6ad692a584e016d085e746ad4eb561068e
WebSavva <websavvap@gmail.com>
2025-10-12 00:16:47 +0300

    feat(editor): EditorFooter was implemented

---
+++ app/components/Editor/Footer/index.server.tsx
@@ -0,0 +1,142 @@
+import { NuxtIcon } from '#components';
+
+export default defineComponent({
+  name: 'EditorFooter',
+  props: {
+    class: {
+      type: String,
+      default: '',
+    },
+  },
+
+  setup(props) {
+    return () => (
+      <footer
+        class={cn(
+          'flex border-t-2 border-editor-border text-editor-fg space-x-1',
+          props.class,
+        )}
+      >
+        <button
+          class={cn(
+            'py-1 px-2 bg-blue-400/50 hover:bg-editor-background-secondary transition text-white',
+          )}
+        >
+          <NuxtIcon
+            name="codicon:remote"
+            class={cn('size-4.5')}
+          />
+        </button>
+
+        <button
+          class={cn(
+            'py-1 px-2 flex items-center hover:bg-editor-background-secondary transition',
+          )}
+        >
+          <NuxtIcon
+            name="codicon:source-control"
+            class={cn('size-4.5')}
+          />
+
+          <span class={cn('ml-1 text-sm')}>main*</span>
+        </button>
+
+        <button
+          class={cn(
+            'py-1 px-2 flex items-center hover:bg-editor-background-secondary transition',
+          )}
+        >
+          <NuxtIcon
+            name="codicon:cloud-upload"
+            class={cn('size-5')}
+          />
+        </button>
+
+        <button
+          class={cn(
+            'py-1 px-1.5 flex items-center space-x-1 hover:bg-editor-background-secondary transition mr-auto',
+          )}
+        >
+          <NuxtIcon
+            name="codicon:error"
+            class={cn('size-4')}
+          />
+
+          <span class={cn('text-sm ml-1')}>0</span>
+
+          <NuxtIcon
+            name="codicon:warning"
+            class={cn('size-4')}
+          />
+
+          <span class={cn('text-sm ml-1')}>0</span>
+        </button>
+
+        <button
+          class={cn(
+            'py-1 px-2 flex items-center hover:bg-editor-background-secondary transition text-sm',
+          )}
+        >
+          Spaces: 2
+        </button>
+
+        <button
+          class={cn(
+            'py-1 px-2 flex items-center hover:bg-editor-background-secondary transition text-sm',
+          )}
+        >
+          UTF-8
+        </button>
+
+        <button
+          class={cn(
+            'py-1 px-2 flex items-center hover:bg-editor-background-secondary transition text-sm',
+          )}
+        >
+          CRLF
+        </button>
+
+        <button
+          class={cn(
+            'py-1 px-2 flex items-center hover:bg-editor-background-secondary transition text-sm',
+          )}
+        >
+          <NuxtIcon
+            name="codicon:json"
+            class={cn('size-4')}
+          />
+
+          <span class={cn('text-sm ml-1')}>
+            TypeScript JSX
+          </span>
+        </button>
+
+        <button
+          class={cn(
+            'py-1 px-2 flex items-center hover:bg-editor-background-secondary transition text-sm',
+          )}
+        >
+          <NuxtIcon
+            name="codicon:broadcast"
+            class={cn('size-4')}
+          />
+
+          <span class={cn('text-sm ml-1')}>
+            Go Live
+          </span>
+        </button>
+
+        <button
+          class={cn(
+            'py-1 px-2 flex items-center hover:bg-editor-background-secondary transition mr-2',
+          )}
+        >
+          <NuxtIcon
+            name="codicon:bell-dot"
+            class={cn('size-5')}
+          />
+        </button>
+      </footer>
+    );
+  },
+});
--- app/components/Editor/index.tsx
+++ app/components/Editor/index.tsx
@@ -1,4 +1,4 @@
-import { EditorTopBar, NuxtIcon } from '#components';
+import { EditorTopBar, EditorFooter } from '#components';
 
 export default defineComponent({
   name: 'Editor',
@@ -12,10 +12,12 @@ export default defineComponent({
   setup(props, { slots }) {
     return () => {
       return (
-        <div class={cn('flex flex-col', props.class)}>
+        <div class={cn('flex flex-col h-screen', props.class)}>
           <EditorTopBar class={cn('w-full')} />
 
-          {slots.default?.()}
+          <div class={cn('flex-1')}>{slots.default?.()}</div>
+
+          <EditorFooter class={cn('w-full')} />
         </div>
       );
     };
