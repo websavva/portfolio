27ea43d25da01089ab93ca810ee4be487e5ad7c7
WebSavva <websavvap@gmail.com>
2025-11-04 00:42:05 +0300

    feat: Puppeeteer args was set

--- apps/cv/scripts/build.ts
+++ apps/cv/scripts/build.ts
@@ -29,6 +29,12 @@ async function build() {
 
   const browser = await puppeteer.launch({
     executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
+    ],
   });
   const page = await browser.newPage();
 
